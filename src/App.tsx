8d339a109f6216e9235d155325a60c85aefde9a2
localhostd3veloper <gautamanand022@gmail.com>
2023-09-26 17:04:58 +0530

    basic layout and effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+interface NoticesState {
+  date_of_birth: string;
+  entity_id: string;
+  forename: string;
+  name: string;
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [notices, setNotices] = useState<Array<NoticesState>>([]);
+  const [search, setSearch] = useState<
+    string | number | readonly string[] | undefined
+  >("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getNotices = async () => {
+      setLoading(true);
+      await fetch(
+        `https://ws-public.interpol.int/notices/v1/red?forename=${search}&resultPerPage=200`
+      )
+        .then((res) => res.json())
+        .then((json) => setNotices(json._embedded.notices));
+      setLoading(false);
+    };
+    getNotices();
+  }, [search]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen bg-gray-200 flex flex-col gap-3 px-10 pt-5">
+      <div className="text-4xl font-semibold">React Debouncing Demo</div>
+      <div className="flex-1 flex flex-col justify-start items-center">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          className="outline-none focus:outline-blue-800 shadow-md hover:shadow-lg duration-700 px-4 py-2 h-11 rounded-md md:w-1/3 placeholder:animate-pulse "
+          placeholder="🔍 Search Anything..."
+          value={search}
+        />
+        <div> {!loading && notices.map((notice) => <span key={notice.entity_id} className="px-2">{notice.name}</span>)}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
